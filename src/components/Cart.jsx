78c5123d9cdb151df2580cb0b29b7d1572fba143
Ericawanja <wanjaerica@gmail.com>
2022-11-19 00:48:14 +0300

    cart ui

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { add_cart, remove_cart } from "../redux/slices/cart_slice";
+
 
 function Cart() {
+  const dispatch = useDispatch()
   const { cart_list, cart_value } = useSelector((state) => state.cart);
   return (
     <div className="cartWrapper">
       <div className="inner_cart_wrapper">
+        <h2>Cart items</h2>
         {cart_list.length > 0 ? (
           cart_list.map((item) => {
             let { id, name, description, price, discount, imageUrl } = item;
@@ -19,18 +23,38 @@ function Cart() {
                   </div>
                   <div className="cart_product_details">
                     <span className="cart_p_title">{name}</span>
-                    <span className="cart_p_price">{price}</span>
+                    <span className="cart_p_price">Price:{price}</span>
                     <span className="cart_p_desc">{description}</span>
                   </div>
                 </div>
-                <div className="cart_right"></div>
+                <div className="cart_right">
+                  <span className="cart_btn extra_btns">
+                    <span
+                      className="plus_cart_icon"
+                      onClick={() => dispatch(add_cart(item))}
+                    >
+                      {" "}
+                      +{" "}
+                    </span>
+                    <span>0</span>
+                    <span
+                      className="minus_cart_icon"
+                      onClick={() => dispatch(remove_cart(id))}
+                    >
+                      {" "}
+                      -
+                    </span>
+                  </span>
+                </div>
               </div>
             );
           })
         ) : (
           <span className="empty_cart">
             <h1>Nothing in the cart</h1>
-            <Link to="/products" className="go_back"><button >Go Back</button></Link>
+            <Link to="/products" className="go_back">
+              <button>Go Back</button>
+            </Link>
           </span>
         )}
       </div>
